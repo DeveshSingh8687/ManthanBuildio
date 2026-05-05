824189b80b31599789f1ba5d43ce1a716f823c9a
Manthan006-hub <hunterx006i@gmail.com>
2026-05-05 21:59:12 +0530

    Ist commitr

--- screen/JobsSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import TopTabsComponent from './components/TabComponent'
-
- const JobsSection = () => {
-  return (
-    <TopTabsComponent/>
-  )
-}
-export default JobsSection
